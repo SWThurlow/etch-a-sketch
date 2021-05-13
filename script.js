ca4e0462f48494f3f79ad863889c5b0378da7ac3
Samuel Thurlow <thurlow.webstuff@gmail.com>
2021-05-13 08:27:08 +0100

    ADD BORDER TO GRID CONTAINER AND SET HTML ELEMENTS AS VARIABLES IN SCRIPT
    Added border to the grid container div and set it's height and width.
    
    Set the html elements needed in the javascript to variables and made
    the colours object.
    
    Also amended some typo's in index.html and changed a class name to make
    more sense.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
- 
+ /*Retrieve HTMl elements*/
+ const container = document.querySelector('.etchCanvas');
+ const gridSize = document.getElementById('gridSize');
+ const greyScale = document.getElementById('greyScale');
+ const rainbow = document.getElementById('rainbow');
+ const colourPicker = document.getElementById('picker');
+ const clearGrid = document.getElementById('clearGrid');
+ const eraser = document.getElementById('eraser')
+
+ /*Colours object*/
+const colours = {
+    picker: colourPicker.nodeValue,
+    rainbow: false,
+    grey: false,
+    eraser: false,
+}
+
